d71d3d0b24049b58a0a46529d6028583e5fd98e4
Anthony Tucto <anthony.tucto.m@gmail.com>
2020-09-01 10:27:23 -0500

    Agregado Migraciones Cami

---
+++ database/migrations/2020_09_01_091323_create_cami_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCamiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cami', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('logo',100);
+            $table->string('title_es');
+            $table->string('title_en');
+            $table->text('description_es');
+            $table->text('description_en');
+            $table->string('title_team_es');
+            $table->string('title_team_en');
+            $table->text('description_team_es');
+            $table->text('description_team_en');
+            $table->string('title_proyects_es');
+            $table->string('title_proyects_en');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cami');
+    }
+}
---
+++ database/migrations/2020_09_01_091400_create_cami_elements_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCamiElementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cami_elements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->string('icon',100);
+            $table->text('description_es');
+            $table->text('description_en');
+            $table->string('image',100);
+            $table->integer('index');
+            //Title, Icon, Short description
+            $table->json('elements_es');
+            $table->json('elements_en');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cami_elements');
+    }
+}

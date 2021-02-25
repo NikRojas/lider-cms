1a6a1808940941d16a2d4aa83c92fbd36038f133
Anthony T <anthony.tucto.m@gmail.com>
2021-02-25 09:31:23 -0500

    Updated Errors

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,7 @@
         <div>
             
             <h1 class="text-center nonecase-font mb-0 block error-comment display-2">Página No Encontrada</h1>
-            <p class="text-center  mb-5">Ha ocurrido un error. Por favor intenta de nuevo.</p>
+            <p class="text-center  mb-5">No pudimos encontrar la página que estabas buscando.</p>
             <div class="row">
                 <div class="col-12 text-center">
                     <a href="{{ route('cms.dashboard' )}}" class="btn btn-primary">Ir al Inicio</a>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,7 +3,7 @@
     <div style="height: 100vh;" class="d-flex align-items-center justify-content-center">
         <div>
         <h1 class="text-center nonecase-font mb-0 block error-comment display-2">Error Interno del Servidor</h1>
-            <p class="text-center  mb-5">No pudimos encontrar la página que estabas buscando.</p>
+            <p class="text-center  mb-5">Ha ocurrido un error. Por favor intenta de nuevo.</p>
             <div class="row">
                 <div class="col-12 text-center">
                     <a href="{{ route('cms.dashboard' )}}" class="btn btn-primary">Ir al Inicio</a>

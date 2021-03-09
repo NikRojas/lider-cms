d4a974d0e5d57bc39667811f5e18f94a347b01dd
Anthony T <anthony.tucto.m@gmail.com>
2021-03-09 10:53:54 -0500

    Actualizado URL Video

--- app/CamiElement.php
+++ app/CamiElement.php
@@ -14,8 +14,18 @@ class CamiElement extends Model
     public function getIdVideoAttribute()
     {
         if ($this->url_video) {
-            $id = explode("?v=", $this->url_video);
-            return $id[1];
+            /*$id = explode("?v=", $this->url_video);
+            return $id[1];*/
+            if(strpos($this->url_video, "?v=") !== false){
+                $id = explode("?v=", $this->url_video);
+                return $id[1];
+            } else{
+                $parsed = parse_url($this->url_video);
+                $path = $parsed['path'];
+                $path_parts = explode('/', $path);
+                $desired_output = $path_parts[1];
+                return $desired_output;
+            }
         }
     }
 

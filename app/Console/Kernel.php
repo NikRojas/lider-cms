331ef9f01c182644caf56b594c2a3b9be183f6ef
Anthony Tucto <anthony.tucto.m@gmail.com>
2021-05-17 11:47:35 -0500

    Added Command Path to Variable

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\SapLogin::class,
-        Commands\SapGetAvailableDepartments::class,
+        App\Console\Commands\SapLogin::class,
+        App\Console\Commands\SapGetAvailableDepartments::class,
     ];
 
     /**

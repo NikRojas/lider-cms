f8f6bc090625d242dbc9f0e13ec7107a89033d66
Anthony Tucto <anthony.tucto.m@gmail.com>
2021-05-17 12:49:46 -0500

    Updated Path COmmands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,10 +15,10 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\SapLogin::class,
-        Commands\SapGetAvailableDepartments::class,
-        //'App\Console\Commands\SapLogin',
-        //'App\Console\Commands\SapGetAvailableDepartments'
+        //Commands\SapLogin::class,
+        //Commands\SapGetAvailableDepartments::class,
+        'App\Console\Commands\SapLogin',
+        'App\Console\Commands\SapGetAvailableDepartments'
     ];
 
     /**

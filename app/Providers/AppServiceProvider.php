6aad9cfedcec7f5d730980c13a981206c6502282
Anthony Tucto <anthony.tucto.m@gmail.com>
2021-07-07 14:55:14 -0500

    Updated Menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -47,7 +47,7 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('layouts.dashboard',function($view){
             $menu = [];
             //$modules = Module::where('name','!=','Ventas y Estad.')->where('name','!=','Clientes')->where('name','!=','Postulantes')->where('name','!=','Libro de Reclamaciones')->get();
-            $modules = Module::where('name','!=','Postulantes')->get();
+            $modules = Module::where('name','!=','Postulantes')->where('name','!=','Libro de Reclamaciones')->get();
             foreach( $modules as $i => $module){
                 if(!$module->parent){  
                     $menu[$i]["id"] = $module->id;

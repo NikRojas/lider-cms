37dbdacf6b79ee27a9e135c7969ab95bf1a63346
Anthony <anthony.tucto.m@gmail.com>
2021-08-27 18:05:34 -0500

    Actualizado Observaciones 27

--- app/Http/Controllers/Api/Reserve/PostController.php
+++ app/Http/Controllers/Api/Reserve/PostController.php
@@ -180,14 +180,14 @@ class PostController extends BaseController
                 ],
             ],
             "metadata" => [
-                "tipo_documento" => $request->type_document_id,
+                "Tipo Documento" => $request->type_document_id,
                 "Nombres" => $request->name,
                 "Número de Documento" => $request->document_number,
                 "Apellido Paterno" => $request->lastname,
                 "Apellido Materno" => $request->lastname_2,
                 "Proyecto" => $department->projectRel->name_es,
                 "Inmueble" => $department->description,
-                "cod_inmueble" => $department->sap_code
+                "Código Inmueble" => $department->sap_code
             ]
         ];
         $authToken = $credentialPayment->user.':'.$credentialPayment->password_prod;

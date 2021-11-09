4ff829c5c18e1012030f86bccccc97ec48324a68
Anthony T <anthony527@gmail.com>
2021-11-09 17:20:19 -0500

    Actualizaco Updating Columns 2

--- database/migrations/2021_11_09_171815_updating_columns_2_to_departments_table.php
+++ database/migrations/2021_11_09_171815_updating_columns_2_to_departments_table.php
@@ -15,9 +15,9 @@ class UpdatingColumns2ToDepartmentsTable extends Migration
     {
         Schema::table('departments', function (Blueprint $table) {
             $table->bigInteger('view_id')->nullable()->unsigned()->change();
-            $table->foreign('view_id')->references('id')->on('projects_views');
+            //$table->foreign('view_id')->references('id')->on('projects_views');
             $table->bigInteger('type_department_id')->nullable()->unsigned()->change();
-            $table->foreign('type_department_id')->references('id')->on('project_type_departments');
+            //$table->foreign('type_department_id')->references('id')->on('project_type_departments');
         });
     }
 
